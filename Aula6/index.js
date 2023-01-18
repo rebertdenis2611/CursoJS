b36cc87e22760986ba1a9361c008041435346a2b
rebertdenis2611 <rebertdenis@hotmail.com>
2023-01-18 18:27:09 -0300

    Atualizão do curso

--- Aula6/index.js
+++ Aula6/index.js
@@ -1,3 +1,14 @@
+/* Não podemos criar variáveis com pavras reservadas
+    Variáveis precisam de ter nomes significativos
+    Não pode começar o nome de uma variável com um número
+    Não podem conter espaçõs ou traços
+    utilizamos camlCase
+    Case-sensitive
+    Não podemos redeclarar variáveis com let
+    NÃO UTILIZE VAR, UTILIZE LET.
+*/
+
+
 var nomecliente = 'REbert';
 var nomecliente = 'REbert';
 
---
+++ Aula7/index.js
@@ -0,0 +1,10 @@
+const primeiroNumero = 2;
+const segundoNumero = 5;
+const soma = primeiroNumero * segundoNumero;
+
+console.log(soma)
+
+console.log( typeof soma)
+
+
+
