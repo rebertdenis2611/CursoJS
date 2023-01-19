c73417ed13378c54e1e7d4c8f823ce697adeb6d0
rebertdenis2611 <rebertdenis@hotmail.com>
2023-01-19 18:16:14 -0300

    Atualizçaão
    Aulas de Atribuição e Incremento

---
+++ Aula10/index.js
@@ -0,0 +1,8 @@
+const n1 = 10;
+const n2 = Number('7');
+
+console.log(n1 + n2)
+console.log(typeof n2)
+n1++  // Incremento
+n1-- // Decremento
+n1+= n2 // Atribuição
--- Aula9/index.js
+++ Aula9/index.js
@@ -1,4 +1,11 @@
-const nome = ('Rebert');
-console.log(nome)
-nome = ('Denis');
-console.log(nome)
+// String, Number, undefined, null, Bolean
+
+const nome1 = ('Rebert') // String
+const nome2 = ("Rebert") // String
+const nome3 = (`Rebert`) // String é usado também para template string
+const num = 3; //number
+const num1 = 10.5; //number
+let vazio; // undefine ->  não ponta para nenhum lugar. (Aqui o motor do javascript que define)
+const sobreNome = null; //NULL não ponta para nenhum lugar. (Aqui eu que defino) 
+const op = true; //bolean
+const op1 = false; //bolean
