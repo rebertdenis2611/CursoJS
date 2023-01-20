8119be518441373f1f34ea5830f1cca7804d9296
rebertdenis2611 <rebertdenis@hotmail.com>
2023-01-19 23:16:19 -0300

    Aulas de concatenação
    Aulas de concatenação e atribuição devalores.

---
+++ Aula11/js/index.js
@@ -0,0 +1,8 @@
+let n1 = prompt('Digite o primeiro número: ');
+let n2 = prompt('Digite o segundo número: ');
+
+n1 = Number(n1)
+n2 = Number(n2)
+
+alert(`O resultado da conta foi ${n1 + n2}`);
+confirm('Confirma as informações? ')
---
+++ Aula13/index.js
@@ -0,0 +1,6 @@
+let nomeCasa = "Casa grande "
+console.log(nomeCasa.concat(' Olá minha casa'))
+console.log(nomeCasa + ' Olá minha casa')
+console.log(`${nomeCasa} Olá minha casa`)
+console.log(nomeCasa.indexOf('nde', 3))
+console.log(nomeCasa.lastIndexOf('nde', 9))
