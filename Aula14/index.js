d1b23dab45f7c82753a4164f43b205132586cd50
rebertdenis2611 <rebertdenis@hotmail.com>
2023-01-21 21:25:13 -0300

    Refazendo a aula
    Refazendo a aula

--- Aula14/index.js
+++ Aula14/index.js
@@ -1,3 +1,8 @@
+
+
+
+
+
 let num1 = 0.7;
 let num2 = 0.1;
 
@@ -5,7 +10,7 @@ console.log(num1 += num2);
 console.log(num1 += num2);
 console.log(num1 += num2);
 
-num1 = Number(num1.toFixed(2))
+num1 = Number(num1.toFixed(2));
 
 console.log(num1)
 
