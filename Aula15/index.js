c9b2c417784554e555fd391bee6bd8cadcae47de
rebertdenis2611 <rebertdenis@hotmail.com>
2023-01-22 09:48:44 -0300

    Update index.js

--- Aula15/index.js
+++ Aula15/index.js
@@ -1,1 +1,10 @@
-Math.
+let num1 = 9.51947;
+
+//let num2 = Math.floor(num1); //arredonda para baixo
+//let num2 = Math.ceil(num1); //arredonda para cima
+//let num2 = Math.round(num1); //arredonda para o mais proximo 
+//console.log(Math.max(1,2,3,4,5)); //mostra o maior número 
+//console.log(Math.min(1,2,3,4,5,6,7,8,-23,-43,-54)); //mostra o menor número.
+/*const aleatorio = (Math.round(Math.random() * (50 - 20)+20))
+console.log(aleatorio)*/
+console.log(Math.PI);
