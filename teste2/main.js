54e97c55d0be613c31edb8384e404e84c2783618
rebertdenis2611 <rebertdenis@hotmail.com>
2023-06-21 23:22:30 -0300

    Atualização testes

--- teste2/main.js
+++ teste2/main.js
@@ -1,17 +1,21 @@
-const botao = document.querySelector('#botao');
-let para = document.querySelector('p')
-//const n1 = parseInt(prompt('Digite o primeiro número para começar a contagem: '))
-//const n2 = parseInt(prompt('Digite até onde iremos contadr: '))
-const nomes = {
-    nome: 'Rebert',
-    idade: 27
+const login = document.querySelector("#login");
+const senhaDigitada = document.querySelector("#senha");
+const avisos = document.querySelector("#avisos");
+const entrar = document.querySelector("#entrar");
+
+const email = 'a';
+const senha = '123';
+
+entrar.addEventListener('click',function(){
+    validaCredencial()
+})
+
+function validaCredencial(){
+    if (login.value === email && senhaDigitada.value === senha){
+        avisos.innerHTML = 'Login Validado, BRENO amanhã para o lanche e chupa alguém lá de baixo e eduardo paga a guaraná e lambe dona rose.';
+        avisos.className = 'validado';
+    }else{
+        avisos.innerHTML = 'Login invalido';
+        avisos.className = 'naovalidado';
+    }
 };
-
-for(let i in nomes){
-    para.innerHTML += i;
-}
-
-
-//for(let i ; i < n2; i++){
- //   para.innerHTML += `<br>${i}`;
-//}
