ee3779cc9d57c8afb277bc120dd7f3ca758dd1f2
rebertdenis2611 <rebertdenis@hotmail.com>
2023-06-25 22:45:41 -0300

    Alteracaoes

--- teste2/main.js
+++ teste2/main.js
@@ -2,20 +2,29 @@ const login = document.querySelector("#login");
 const senhaDigitada = document.querySelector("#senha");
 const avisos = document.querySelector("#avisos");
 const entrar = document.querySelector("#entrar");
+const boasVindas = document.querySelector("#boasvindas");
 
-const email = 'a';
+const email = 'rebert';
 const senha = '123';
 
 entrar.addEventListener('click',function(){
-    validaCredencial()
+    validaCredencial();
 })
 
 function validaCredencial(){
-    if (login.value === email && senhaDigitada.value === senha){
-        avisos.innerHTML = 'Login Validado, BRENO amanhã para o lanche e chupa alguém lá de baixo e eduardo paga a guaraná e lambe dona rose.';
-        avisos.className = 'validado';
+    if (login.value === email && senhaDigitada.value === senha){    
+        location = "tela2.html"
+        avisos.innerHTML = `Login validado com sucesso`;
+        avisos.className = 'validado';   
+        escrevaSaldacao();
     }else{
-        avisos.innerHTML = 'Login invalido';
+        avisos.innerHTML = 'Login ou senha errada';
         avisos.className = 'naovalidado';
+        escrevaSaldacao();
     }
+};
+
+function escrevaSaldacao(){
+    boasVindas.innerHTML = `Bem vindo ${login}, hoje o dia está lindo!`;
+    console.log('hoje o dia está lindo!');
 };
