d22e9d8123410e63e292b8911e0c8ab9664e3f4c
rebertdenis2611 <rebertdenis@hotmail.com>
2023-08-17 20:13:20 -0300

    Estudos de callback

--- Testes/index2.js
+++ Testes/index2.js
@@ -1,28 +1,32 @@
-function f1(callback){
-    console.log('f1')
-    if(callback) callback()
+function criarPessoa(nome, sobrenome, peso, altura){
+    return{
+        nome,
+        sobrenome,
+
+        get nomeCompleto(){
+            return `${this.nome} ${this.sobrenome}`
+        },
+
+        set nomeCompleto(valor){
+            valor = valor.split(' ');
+            this.nome = valor.shift();
+            this.sobrenome = valor.join(' ');
+        },
+
+        fala(){
+            return `${nome + ' ' + sobrenome}`
+        },
+        peso,
+        altura,
+
+        get imc(){
+            const indice = peso / (altura ** 2)
+            return indice.toFixed(2)
+        }
+    }
 };
 
-function f2(callback){
-    console.log('f2')
-    if(callback) callback()
-};
-
-function f3(callback){
-    console.log('f3')
-    if(callback) callback()
-};
-
-f1(f1callback);
-
-function f1callback(){
-    f2(f2callback)
-};
-
-function f2callback(){
-    f3(f3callback)
-};
-
-function f3callback(){
-    console.log('OLA MUNDO')
-};
+const p1 = criarPessoa('Rebert', 'Denis', 80, 1.64);
+p1.nomeCompleto = 'Ana Karoline Silva'
+console.log(p1.imc);
+console.log(p1.nomeCompleto);
