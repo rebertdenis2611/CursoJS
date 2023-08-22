a832316c5cd65431d78c9c6a0173fc365ccdf022
Rebert Denis <114620393+rebertdenis2611@users.noreply.github.com>
2023-08-22 18:14:34 -0300

    Conclusão Calculadora.

--- Aula57/assets/js/main.js
+++ Aula57/assets/js/main.js
@@ -1,20 +1,64 @@
 function criarCalculadora(){
     return{
         display: document.querySelector('.display'),
-            
+
         inicia(){
             this.cliqueBotoes();
+            this.pressionaEnter();
+        },
+       
+        pressionaEnter(){
+            this.display.addEventListener('keyup', e =>{
+                if(e.keyCode === 13){
+                    this.realizaConta()
+                }
+            })
+        },
+
+        btnClear(){
+            this.display.value = ' '
+        },
+        
+        apagaUm(){
+            this.display.value = this.display.value.slice(0, -1)
+        },
+
+        realizaConta(){
+            let conta = this.display.value;
+
+            try{
+                conta = eval(conta)
+                if(!conta){
+                    alert('Conta invalida');
+                    return;
+                }
+                this.display.value = String(conta);
+            }catch(e){
+                alert('Conta invalida');
+                return;
+            }
         },
 
         cliqueBotoes(){
-            document.addEventListener('click',function(e){
+            document.addEventListener('click', e =>{
                 const el = e.target;
                 
                 if(el.classList.contains('btn-num')){
                     this.btnParaDisplay(el.innerText);
                 };
 
-            }.bind(this));
+                if(el.classList.contains('btn-clear')){
+                    this.btnClear();
+                }
+
+                if(el.classList.contains('btn-del')){
+                    this.apagaUm();
+                }
+
+                if(el.classList.contains('btn-eq')){
+                    this.realizaConta()
+                }
+            });
         },
 
         btnParaDisplay(valor){
