876cf62e731a1b4c1d1dc7836f2a2ba4c2f84d4d
rebertdenis2611 <rebertdenis@hotmail.com>
2023-08-21 22:48:46 -0300

    Atualização das aulas

--- Aula57/assets/js/main.js
+++ Aula57/assets/js/main.js
@@ -1,3 +1,27 @@
 function criarCalculadora(){
-    
+    return{
+        display: document.querySelector('.display'),
+            
+        inicia(){
+            this.cliqueBotoes;
+        },
+
+        cliqueBotoes(){
+            document.addEventListener('click',function(e){
+                const el = e.target;
+                console.log(this)
+
+                if(el.classList.contains('btn-num')){
+                    this.btnParaDisplay(el.innerText);
+                }
+            });
+        },
+
+        btnParaDisplay(valor){
+            this.display.value += valor
+        }
+    };
 }
+
+const calculadora = criarCalculadora();
+calculadora.inicia();
