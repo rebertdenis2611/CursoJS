1ec86df50403d07e088d931f72224d9e31681c61
rebertdenis2611 <rebertdenis@hotmail.com>
2023-08-21 23:18:07 -0300

    Alteração do c´podigo

--- Aula57/assets/js/main.js
+++ Aula57/assets/js/main.js
@@ -3,18 +3,18 @@ function criarCalculadora(){
         display: document.querySelector('.display'),
             
         inicia(){
-            this.cliqueBotoes;
+            this.cliqueBotoes();
         },
 
         cliqueBotoes(){
             document.addEventListener('click',function(e){
                 const el = e.target;
-                console.log(this)
-
+                
                 if(el.classList.contains('btn-num')){
                     this.btnParaDisplay(el.innerText);
-                }
-            });
+                };
+
+            }.bind(this));
         },
 
         btnParaDisplay(valor){
