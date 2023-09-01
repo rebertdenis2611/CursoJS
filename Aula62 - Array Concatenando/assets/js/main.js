b987e2f0f9bbc1be6dcbc56d0b71a65ce41f821b
rebertdenis2611 <rebertdenis@hotmail.com>
2023-09-01 00:00:44 -0300

    Array Filter

--- Aula62 - Array Concatenando/assets/js/main.js
+++ Aula62 - Array Concatenando/assets/js/main.js
@@ -1,6 +1,4 @@
-const a1 = [1,2,3]
-const a2 = [4,5,6]
-//const a3 = a1.concat(a2, [7,8,9], 'Rebert')
-// ... rest ... spread
-const a3 = [...a1, ...a2]
-console.log(a3);
+const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27];
+let menor = []
+
+for(let x = 0; numeros)
