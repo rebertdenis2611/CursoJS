1f69ac128ef6bd509811777851df16e86fc47c52
Rebert Denis <114620393+rebertdenis2611@users.noreply.github.com>
2023-09-05 18:34:29 -0300

    Filter

--- Aula63 - filter/assets/js/main.js
+++ Aula63 - filter/assets/js/main.js
@@ -1,11 +1,8 @@
 const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27]
-let maior = [];
 
-for(let x = 0; numeros.length; x++){
-    if(numeros[x] > 10 ){
-        maior.push(numeros[x])
-    };  
-};
 
-console.log(maior)
-
+const numerosFiltrados = numeros.filter((valor, indice, array) => {
+    console.log(valor);
+    return valor > 10;
+});
+console.log(numerosFiltrados)
