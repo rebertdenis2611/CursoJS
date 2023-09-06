9b34be74ad08e833188f27b46a22fd1479cf9d70
Rebert Denis <114620393+rebertdenis2611@users.noreply.github.com>
2023-09-06 18:32:55 -0300

    FIlter

--- Aula63 - filter/assets/js/main.js
+++ Aula63 - filter/assets/js/main.js
@@ -1,8 +1,5 @@
-const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27]
-
-
-const numerosFiltrados = numeros.filter((valor, indice, array) => {
+const numeros = [5,6,7,8,59,87,23,11,25,65,48,68,3,4,3];
+const numerosFiltrados = numeros.filter(valor => {
     console.log(valor);
-    return valor > 10;
-});
-console.log(numerosFiltrados)
+    return valor > 10
+})
