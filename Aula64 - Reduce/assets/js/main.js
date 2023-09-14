4cc51a420d5614410adfba3e96ed4573641c64ec
Rebert Denis <114620393+rebertdenis2611@users.noreply.github.com>
2023-09-14 18:40:48 -0300

    a

--- Aula64 - Reduce/assets/js/main.js
+++ Aula64 - Reduce/assets/js/main.js
@@ -1,7 +1,6 @@
 const numeros = [5,6,89,1,2,3,4,5,6,9,75,10,15,27,22]
-const total = numeros.reduce(function(acumulador, valor, indice, array){
+const somaNumeros = numeros.reduce(function(acumulador, valor, indice, array){
     acumulador += valor
     return acumulador
-});
-
-console.log(total)
+}, 0);
+console.log(somaNumeros)
