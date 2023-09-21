d3894d7cc3c2b92eb7e8b1aba50c28a213ec68b8
Rebert Denis <114620393+rebertdenis2611@users.noreply.github.com>
2023-09-21 18:24:14 -0300

    Revisando objeto.

---
+++ Aula68 - Revisando Objetos/assets/js/main.js
@@ -0,0 +1,16 @@
+//const pessoa1 = new Object();
+//pessoa1.nome = 'Rebert';
+//pessoa1.sobrenome = 'Denis';
+//pessoa1.idade = 8
+//pessoa1.falaNome = function(){
+//    return (`${this.nome} está falando seu nome`)
+//};
+//pessoa1.getDataNascimento = function(){
+//    const dataAtual = new Date();
+//    return dataAtual.getFullYear() - this.idade
+//}
+//
+//for(let chave in pessoa1){
+//    console.log(pessoa1[chave])
+//}
+
