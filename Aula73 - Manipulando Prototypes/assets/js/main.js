c04c43ad5d05fe1de20d0a24a84de1a62445d3e7
Rebert Denis <114620393+rebertdenis2611@users.noreply.github.com>
2023-10-09 18:02:44 -0300

    Manipulando Prototypes

--- Aula73 - Manipulando Prototypes/assets/js/main.js
+++ Aula73 - Manipulando Prototypes/assets/js/main.js
@@ -1,10 +1,47 @@
-const objA = {
-    chaveA: 'A'
+function Produto(nome, preco){
+    this.nome = nome;
+    this.preco = preco;
 }
 
-const objB = {
-    chaveB: 'B'
+Produto.prototype.desconto = function(percentual){
+    this.preco = this.preco - (this.preco * (percentual / 100))
 }
 
-Object.setPrototypeOf(objB, objA)
-console.log(objB)
+Produto.prototype.aumento = function(percentual){
+    this.preco = this.preco + (this.preco * (percentual / 100))
+}
+
+const p1 = new Produto('Camiseta', 50)
+
+//Criando um novo Objeto e pegando o PROTO de Produto.
+
+const p2 = {
+    nome: 'Caneca',
+    preco: 5
+};
+Object.setPrototypeOf(p2, Produto.prototype)
+
+p2.aumento(100)
+console.log(p1)
+console.log(p2)
+
+//Pegando o proto e configurando nas linhas
+
+const p3 = Object.create(Produto.prototype);
+p3.preco = 199;
+p3.aumento(100)
+console.log(p3)
+
+
+//Pegando o proto e já configurando.
+const p4 = Object.create(Produto.prototype, {
+    preco : {
+        writable: true,
+        configurable: true,
+        enumerable: true,
+        value: 199    
+    }
+});
+
+p4.aumento(1000)
+console.log(p4)
