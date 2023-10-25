87edbbc6f958078b5312d4a2c4d1e516944c2b8e
Rebert Denis <114620393+rebertdenis2611@users.noreply.github.com>
2023-10-25 18:44:16 -0300

    Aulas

--- teste3/main.js
+++ teste3/main.js
@@ -1,24 +1,7 @@
-function Carro(marca, modelo, ano, dono){
-    this.marca = marca;
-    this.modelo = modelo;
-    this.ano = ano;
-    this.dono = dono;
-    this.mostraCarro = mostraCarro;
-}
+let nome = prompt('Digite apenas 3 caracteres')
 
-function Pessoa(nome, sobrenome, idade){
-    this.nome = nome;
-    this.sobrenome = sobrenome;
-    this.idade = idade;
-}
-
-function mostraCarro(){
-    console.log(`Um belo ${this.marca} ${this.modelo} ${this.ano} ${this.dono}`);
-}
-
-const pesso1 = new Pessoa('Rebert', 'Denis', 28)
-const carro1 = new Carro('Toyota', 'Etios', '2015', pesso1.nome)
-const carro2 = new Carro('Toyota', 'Gol', '9999', pesso1.nome)
-
-carro1.mostraCarro()
-carro2.mostraCarro()
+if(nome.length == 3){
+    alert('Acertou')
+}else{
+    alert('Errado digite apenas 3 caracteres');
+};
