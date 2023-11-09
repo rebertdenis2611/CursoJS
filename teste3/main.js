36e0ca6fb7c7d7ffea2a20ae29042daa323e932b
Rebert Denis <114620393+rebertdenis2611@users.noreply.github.com>
2023-11-09 18:06:50 -0300

    Teste de Gget

--- teste3/main.js
+++ teste3/main.js
@@ -1,29 +1,20 @@
-function Carro(marca, modelo, motor, valor){
-    this.marca = marca;
-    this.modelo = modelo;
-    this.motor = motor;
-    this.valor = valor;
+function Produto(nome, preco, estoque){
+    this.nome = nome;
+    this.preco = preco;
+    Object.defineProperty(this, 'estoque',{
+        enumerable: true,
+        configurable: true,
+        get: function(){
+            return estoque
+        },
+        set: function(valor){
+            estoque = valor;
+        }
+
+    })
+    
 }
 
-Carro.prototype.aumento = function(quantia){
-    this.valor = this.valor + (this.valor * (quantia / 30))
-}
-
-function Config(portas, pneu, velocidade, valor, marca, modelo){
-    Carro.call(this, marca, modelo, valor)
-    this.portas = portas;
-    this.pneu = pneu;
-    this.velocidade = velocidade;
-}
-
-Config.prototype = Object.create(Carro.prototype);
-Config.prototype.constructor = Config;
-
-const carro1 = new Carro('Toyota', 'Etios', 2015, 100000)
-carro1.aumento(100)
-console.log(carro1)
-const carro2 = new Carro('VW', 'Gol', 2019, 100000)
-console.log(carro2)
-const carro3 = new Config(4, 4, 110)
-carro3.aumento(30)
-console.log(carro3)
+const p1 = new Produto('Camisa', 19.99);
+p1.estoque = 50
+console.log(p1)
