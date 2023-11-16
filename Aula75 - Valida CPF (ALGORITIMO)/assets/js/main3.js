505c31fd904bfcc88cf302f8e36f614387fbfb3b
Rebert Denis <114620393+rebertdenis2611@users.noreply.github.com>
2023-11-16 18:27:46 -0300

    Alterações no validado do cpf.

--- Aula75 - Valida CPF (ALGORITIMO)/assets/js/main3.js
+++ Aula75 - Valida CPF (ALGORITIMO)/assets/js/main3.js
@@ -1,18 +1,39 @@
 function validaCpf(cpf){
     return{
         filtrarCpf(){
-            const cpfLimpo = cpf.replace(/\D+/g, '');
-            return cpfLimpo
+            this.cpfLimpo = cpf.replace(/\D+/g, '');
+            this.cpfFiltrado = this.cpfLimpo.slice(0,-2);
+            return this.cpfLimpo
         },
 
         verificaCpf(){
             if(this.filtrarCpf().length !== 11) return false
             if(typeof this.filtrarCpf() === 'undefined') return false
+            
             return true
-        }
+        },
+
+        gerarDigito(){
+            const cpfArray = Array.from(this.filtrarCpf())
+            let regresso = cpfArray.length -1
+            console.log(cpfArray) //funcionou
+            cpfArray.reduce((ac, valor) => {
+                ac += (regresso * Number(valor));
+                regresso--;
+                return ac
+            },0)
+            
+        },
         
+        mostrarMensagem(){
+            if(this.verificaCpf() === true){
+                return('CPF VALIDADO!')
+            }else{
+               return('CPF INVALIDO!')
+            };
+        }
     };
 };
 
 const confereCpf = validaCpf('104.989.884-22');
-console.log(confereCpf.verificaCpf())
+console.log(confereCpf.gerarDigito())
