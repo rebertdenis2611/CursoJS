495f89120785c086cfd98452a9af9772fa275861
Rebert Denis <114620393+rebertdenis2611@users.noreply.github.com>
2023-12-05 18:20:05 -0300

    Alteração Classe

--- Aula78 - Classes/assets/js/main.js
+++ Aula78 - Classes/assets/js/main.js
@@ -1,24 +1,9 @@
-class Pessoa{
-    constructor(nome, sobrenome){
-        this.nome = nome;
-        this.sobrenome = sobrenome;
-    }
+class Animal(){
+  constructor(nome){
+    this.nome = nome
+  }
 
-    falar(){
-        console.log(`${nome} ${sobrenome} bom dia!`)
-    }
+  Float32Array(){
+    console.log(this.nome + "Emite um barulho;")
+  }
 }
-
-function Pessoa2(nome, sobrenome){
-    this.nome = nome;
-    this.sobrenome = sobrenome;
-}
-
-
-
-
-const p1 = new Pessoa('Rebert', 'Denis')
-const p2 = new Pessoa2('Eduardo', 'Denis')
-
-console.log(p1)
-console.log(p2)
