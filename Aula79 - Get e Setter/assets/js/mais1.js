7b32c4ff41e4e3f8b075bd0dd80f8eade582cc7d
Rebert Denis <114620393+rebertdenis2611@users.noreply.github.com>
2023-12-19 18:44:44 -0300

    Classes

--- Aula79 - Get e Setter/assets/js/mais1.js
+++ Aula79 - Get e Setter/assets/js/mais1.js
@@ -1,34 +1,33 @@
+const _velocidade = Symbol();
 class Carro{
     constructor(nome){
         this.nome = nome;
-        this.velocidade = 0;
+        this[_velocidade] = 0;
     }
 
-    acelerar(vel){
-        if(this.velocidade + vel > 220){
-            console.log(`Atingiu a velocidade maxima de ${this.velocidade} não pode mais acelerar`)
-        }else{
-            this.velocidade += vel
-        }
-        
+    get velocidade(){
+        console.log('GETTER')
+        return this[_velocidade];
     }
 
-    freiar(vel){
-        if(this.velocidade === 0){
-            console.log(`Atingiu a velocidade minima de ${this.velocidade} pode soltar o freio e puxar o freio de mão`)
-        }else if(this.velocidade < vel){
-            console.log(`O valor digitado foi superior a velocidade! FREIE MAIS DEVAGAR`)
-        }else{
-            this.velocidade -= vel
-        }
-        
+    set velocidade(kmh){
+        console.log('SETTER')
+        if(kmh > 100 || kmh < 0) return
+        if(typeof kmh !== 'number') return
+        this[_velocidade] = kmh;
+    }
+
+    freiar(valor){
+        this[_velocidade] -= valor;
+        return this[_velocidade];
+    }
+
+    acelerar(valor){
+        this[_velocidade] += valor;
+        return this[_velocidade];
     }
 }
 
-const c1 = new Carro('Toyota')
-c1.acelerar(210)
-console.log(c1)
-c1.freiar(211)
-console.log(c1)
-c1.freiar(212)
-console.log(c1)
+const c1 = new Carro('Toyota');
+c1.velocidade = 101
+console.log(c1.velocidade)
--- CadastroUsuario/assets/js/main.js
+++ CadastroUsuario/assets/js/main.js
@@ -1,18 +1,38 @@
-const usuario = prompt('Qual o nome do usuario?')
-const razaoSocial = document.querySelector('.razaoSocial')
+const usuario = document.querySelector('.nome');
+const password = document.querySelector('.senha');
 
-function entrarSistema(){
-    saudaUsuario();
-    validaRazaoSocial();
-}
 
-function saudaUsuario(usuario){
-    const saudacaoUsuario = document.querySelector("#nomeUsuario")
-    saudacaoUsuario.innerHTML = `${usuario} seja bem vindo!`;
-}
+const mensagem = document.querySelector('.mensagem');
 
-function validaRazaoSocial(){
-    if(typeof razaoSocial != String){
-        alert('Erro!')
+
+class User{
+    constructor(nome, senha){
+        this.nome = nome;
+        this.senha = senha;
+    };
+    
+    valida(){
+        if(this.nome === usuario.value && this.senha === password.value){
+            mensagem.innerHTML = 'Senha e usuario OK'
+            mensagem.className='senha-validada'          
+        }else{
+            mensagem.innerHTML = 'Erro na senha ou usuario'
+            mensagem.className='senha-errada'
+        }
+        e.preventDefault();
+        console.log(this.usuario, this.password);
+    }
+
+    entrarSistema(){
+        const botao = document.querySelector('.salvar')
+        botao.addEventListener('click', function(e){
+            console.log(usuario.value, password.value);
+            console.log(this.nome, this.senha);
+            e.preventDefault();
+        })
+        
     }
 }
+
+const user1 = new User('rebert', 123);
+user1.entrarSistema()
