dba95f3a303cf6b1ee9b56b4fc18063765a50026
Rebert Denis <114620393+rebertdenis2611@users.noreply.github.com>
2024-01-11 23:56:51 -0300

    A

--- Aula83 - Validando Formulario/assets/js/main.js
+++ Aula83 - Validando Formulario/assets/js/main.js
@@ -24,18 +24,21 @@ class validaFormulario{
 
         for(let campo of this.formulario.querySelectorAll('.validar')){
             const label = campo.previousElementSibling.innerHTML;
-
             if(!campo.value){
                 this.mensagemErro(campo, `O campo ${label} não pode ser vázio`);
-                this.valid = false
+                valid = false
             }
         }
+
+        if(campo.classList.contains('usuario')) {
+            if(!this.validaUsuario(campo)) valid = false;
+          }
         return valid
     }
 
     validaUsuario(campo){
         let valid = true;
-        let usuario = this.formulario.querySelector('.usuario')
+        let usuario = campo.value
         if(usuario.length < 3 || usuario.length > 12){
             this.mensagemErro(campo, 'Tamanho do usuário não permitido.')
             valid = false
