b53e432ede338c01a036ea678f9f7d4e101fc224
Rebert Denis <114620393+rebertdenis2611@users.noreply.github.com>
2024-01-24 18:31:06 -0300

    promisses

--- Aula84 - Promisses/assets/js/main.js
+++ Aula84 - Promisses/assets/js/main.js
@@ -1,12 +1,22 @@
 function rand(min, max){
   min *= 1000;
   max *= 1000;
-  return Math.floor(Math.random() * (max - min) + min); 
-}
+
+  return Math.floor(Math.random() * (max - min) + min);
+};
 
 function esperaAi(msg, tempo){
-  return new Promise((resolve, reject));
-  setTimeout(() => {
-    console.log(msg);
-  }, tempo)
+  return new Promise((resolve, reject) => {
+    setTimeout(() =>{
+      resolve(msg)
+    }, tempo)
+  })
 }
+
+esperaAi('Frase 1', rand(1,3))
+  .then(resposta =>{
+    console.log(resposta)
+  })
+  .then(esperaAi('Frase 2', rand(1,3)){
+    onsole.log(resposta)
+  })
