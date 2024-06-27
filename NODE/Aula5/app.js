985f08a958c59f479038dd6f4a5a6e2ac95ee507
Rebert Denis <114620393+rebertdenis2611@users.noreply.github.com>
2024-06-27 12:07:35 -0300

    Escrevendo arquivos.

--- NODE/Aula5/app.js
+++ NODE/Aula5/app.js
@@ -1,18 +1,16 @@
 const path = require('path')
 const caminhoArquivo = path.resolve(__dirname, 'pessoas.json')
+const escreve = require('./modules/escrever')
+const ler = require('./modules/ler')
 
 //implementaçoes
 const caminhoArquivoLog = path.resolve(__dirname, 'arquivolog.txt')
 
-const escreve = require('./modules/escrever')
-const ler = require('./modules/ler')
-const escrever = require('./modules/escrever')
-
 //tentar colocar horario
-const horario = require('./modules/horario')
+//const horario = require('./modules/horario')
 // console.log(horario)
 
-
+        //ESCREVE NO ARQUIVO ESCREVER.JS AS INFORMAÇÕES ABAIXO.
 // const pessoas = [
 //     {nome: 'Rebert'},
 //     {nome: 'Luiz'},
@@ -20,19 +18,30 @@ const horario = require('./modules/horario')
 //     {nome: 'José'}
 // ]
 
-//const json = JSON.stringify(pessoas, '', 2)
-//escreve(caminhoArquivo, json)
+// const json = JSON.stringify(pessoas, '', 2)
+// escreve(caminhoArquivo, json)
+
+//PEGA INFORMAÇÕES DA LEITURA DO ARQUIVO.
 async function leArquivo(caminho){
     const dados = await ler(caminho);
+    renderizaDados(dados)
     return dados
 }
 
-const dadosArquivos = leArquivo(caminhoArquivo)
-    .then(dados =>{
-        console.log(dados)
-    })
-    .catch(e =>{
-        escrever(caminhoArquivoLog,`O arquivo não foi encontrado`)
-        //implementaçoes
-        //escreve(caminhoArquivoLog, `${horario} : Erro ao lê arquivo de dados ` )
-    })
+//PARA NÃO FAZER ISSO PODE-SE CRIAR UMA NOVA FUNÇÃO PARA NÃO CRIAR A FUNÇÃO
+// const dadosArquivos = leArquivo(caminhoArquivo)
+//     .then(dados =>{
+//         console.log(dados)
+//     })
+//     .catch(e =>{
+//         escrever(caminhoArquivoLog,`O arquivo não foi encontrado`)
+//         //implementaçoes
+//         //escreve(caminhoArquivoLog, `${horario} : Erro ao lê arquivo de dados ` )
+//     })
+
+function renderizaDados(dados){
+    dados = JSON.parse(dados)
+    dados.forEach(val => console.log(val.nome))
+}
+
+leArquivo(caminhoArquivo)
