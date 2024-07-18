33278ddb5b93e65da5b062e8dfac406293d9f19e
Rebert Denis <114620393+rebertdenis2611@users.noreply.github.com>
2024-07-18 18:22:16 -0300

    Express(req, resp)

--- NODE/Aula8/server.js
+++ NODE/Aula8/server.js
@@ -4,33 +4,28 @@ const port = 3333;
 
 app.use(express.urlencoded({extended: true}))
 
-app.get('/', (req, resp) =>{
-    resp.send(`
-        <form action="/" method='POST'>
-        Nome do cliente: <input type="text" name="nome">
-        <input type="text" name="outrovalor">
-        <button>ENVIAR O ARQUIVO</button>    
-        </form>
+app.get('/', (req, res) =>{
+    res.send(`
+    <form action="/" method="POST">
+    Digite seu nome: <input type="text" name="nome"></input>
+    <button>ENVIAR</button>
+    </form>
     `)
 })
 
-app.get("/testes/:id_usuarios?/:parametro?/:idade?", (req, res) =>{
-    console.log('Estou no GET')
-    console.log(req.query);
+
+app.post('/', (req, res) =>{
+    res.send('Recebi o arquivo!')
+    console.log(req.body)
 })
 
-
-app.post('/', (req, resp) =>{
-    console.log(req.body);
-    if(req.body.nome === 'rebert'){
-        console.log('Login validado!');
-    }else{
-        console.log('Não validado')
-    }
-    return resp.end()
-    resp.send(`O que você me enviou foi ${req.body.nome} ${req.body.outrovalor}`);
+app.get('/testes/:usuario?/:senha?', (req, res) => {
+    res.send(req.params.usuario)
+    console.log(req.query)
 })
 
-app.listen(port, () =>{
-    console.log('Porta Aberta.');
+app.get('/')
+
+app.listen(port, (req, res) =>{
+    console.log('Estamos escultando!')
 })
