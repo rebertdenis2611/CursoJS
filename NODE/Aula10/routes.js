9a673c540fe67d4db47a4f667d770f830f2adcf7
Rebert Denis <rebertdenis@hotmail.com>
2024-08-01 22:44:52 -0300

    AUla 10

--- NODE/Aula10/routes.js
+++ NODE/Aula10/routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const route = express.Router()
-const home = require('./controllers/controllersHome')
-const contato = require('./controllers/controllersContato')
+const home = require('.src/controllers/controllersHome')
+const contato = require('.src/controllers/controllersContato')
 
 //rota do home
 
---
+++ NODE/Aula10/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const port = 3333;
+const routes = require('./routes')
+const path = require('path')
+
+
+app.use(express.urlencoded({extended: true}))
+app.set('views', path(__dirname, 'src', 'views'))
+app.set('view engine', 'ejs');
+app.use(routes)
+
+app.listen(port, (req, res) =>{
+    console.log('Estamos escultando!')
+    console.log(`http://127.0.0.1:${port}`)
+})
